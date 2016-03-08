e9309f1cd4e3fc5a8c95114b69184614578ee2e5
Lucas Mancini <mancini.lucas@gmail.com>
2016-03-07 22:14:49 -0300

    Introducing the concept of Lanes.

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -3,11 +3,12 @@ import Note from './Note.jsx';
 
 export default ({notes, onEdit, onDelete}) => {
     var notesElements = notes.map(note => 
-            <li key={note.id} className="note">
-                <Note task={note.task} 
-                    onEdit={onEdit.bind(null, note.id)} 
-                    onDelete={onDelete.bind(null, note.id)} />
-            </li>);
+                <li key={note.id} className="note">
+                    <Note task={note.task} 
+                        onEdit={onEdit.bind(null, note.id)} 
+                        onDelete={onDelete.bind(null, note.id)} />
+                </li> 
+            );
         
     return( 
         <ul className="notes">
