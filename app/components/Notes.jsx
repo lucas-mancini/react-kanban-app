50cdc7b1c8bef2099e86b7d346731f237fbb8701
Lucas Mancini <mancini.lucas@gmail.com>
2016-03-11 12:38:36 -0300

    Starting to implement drag and drop.

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Editable from './Editable.jsx';
+import Note from './Note.jsx';
 
 export default ({notes, onValueClick, onEdit, onDelete}) => {
     var notesElements = notes.map(note => 
-                <li key={note.id} className="note">
+                <Note className="note" key={note.id} className="note">
                     <Editable
                         editing={note.editing} 
                         value={note.task} 
                         onValueClick={onValueClick.bind(null, note.id)} 
                         onEdit={onEdit.bind(null, note.id)} 
                         onDelete={onDelete.bind(null, note.id)} />
-                </li> 
+                </Note> 
             );
         
     return( 
